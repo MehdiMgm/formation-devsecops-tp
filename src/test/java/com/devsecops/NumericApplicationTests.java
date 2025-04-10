6d0c1183ef1bf46beabd199bee815930d53d0eda
MehdMgm <il-degahn@hotmail.fr>
2025-04-10 16:53:54 +0200

    Ajout et modif Steps

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -37,13 +37,13 @@ public class NumericApplicationTests {
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Smaller than or equal to 50"));
+                .andExpect(content().string("plus petit que ou egale a 50"));
     }
 
     @Test
     public void greaterThanFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Greater than 50"));
+                .andExpect(content().string("plus grand que : 50"));
     }
     
     @Test
