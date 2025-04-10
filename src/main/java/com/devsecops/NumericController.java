a378cbb04d38c80608a4524807b096d3ffdc5415
MehdMgm <il-degahn@hotmail.fr>
2025-04-10 15:33:07 +0200

    Modif DEPLOYE

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,16 +27,16 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps ZZ test2";
+			return "LE GRAND REMPLACEMENT STARTED";
 		}
 
 		@GetMapping("/compare/{value}")
 		public String compareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
 			if (value > 50) {
-				message = "Greater than 50";
+				message = "plus grand que : 50";
 			} else {
-				message = "Smaller than or equal to 50";
+				message = "plus petit que ou egale a 50";
 			}
 			return message;
 		}
